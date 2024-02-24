5c7e731e2d4352d7708d58a858b1fca330595b47
skiner <fanomy90@gmail.com>
2024-02-24 15:57:34 +0300

    Подготовка главного приложения

---
+++ store/main/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    context: dict = {
+        'title': 'Главная',
+        'content': 'Магазин одежды'
+    }
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context: dict = {
+        'title': 'О нас',
+        'content': 'О нас',
+        'text_on_page': 'Здесь могла быть ваша реклама'
+    }
+    return render(request, 'main/about.html', context)
--- store/store/settings.py
+++ store/store/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.conf.global_settings import STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -105,6 +107,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS: list[Path] = [
+    BASE_DIR / 'static'
+    ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
