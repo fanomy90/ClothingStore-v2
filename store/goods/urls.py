5edcab6428ce6d71e591b6c08ebcfc7a94ae83e0
skiner <fanomy90@gmail.com>
2024-02-27 18:33:52 +0300

    Работа со слагами

--- store/goods/urls.py
+++ store/goods/urls.py
@@ -3,6 +3,6 @@ from goods import views
 app_name = 'goods'
 urlpatterns = [
     path('', views.catalog, name='index'),
-    path('product/', views.product, name='product'),
-
+    path('product/<slug:product_slug>/', views.product, name='product'),
+    #path('product/<int:product_id>/', views.product, name='product'),
 ]
