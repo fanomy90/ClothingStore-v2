d588ee3dcd2233e4aa63d10e7c6e9e78b1b58dd6
skiner <fanomy90@gmail.com>
2024-02-29 16:49:30 +0300

    Добавлены фильтры

--- store/goods/views.py
+++ store/goods/views.py
@@ -3,14 +3,25 @@ from goods.models import Categories, Products
 from django.core.paginator import Paginator
 
 def catalog(request, category_slug):
+    #берем параметры страницы из запроса пользователя, значение по умолчанию 1 страница
     page = request.GET.get('page', 1)
+    #параметры фильтров из get запроса который прописан в шаблоне
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    #делаем вывод товаров по категориям
     if category_slug == 'all':
     #ojects это метод для работы с записями в БД
-    #Использование фильтра при выводе товара по категориям
         goods = Products.objects.all()
     else:
+        #Использование фильтра при выводе товара по категориям
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
-        #goods = Products.objects.filter(category__slug=category_slug)
+    #выполняем проверку параметров переданных через фильтр в шаблоне и присваиваем полученные значения в фильтр и спортировку
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+    if order_by and order_by !="default":
+        goods = goods.order_by(order_by)
+    #благодаря ленивым запросам все пройденные проверки которые формируют queryset сформируют единый SQL запрос к БД и получат соответстующие данные
+
     #пагинатор
     paginator = Paginator(goods, 9)
     current_page = paginator.page(int(page))
