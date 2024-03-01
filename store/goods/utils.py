374c3abec84ea4f8a0af954c08308f7d05c6708f
skiner <fanomy90@gmail.com>
2024-03-01 14:57:41 +0300

    Добавлен полнотекстовый поиск по имени и описанию

--- store/goods/utils.py
+++ store/goods/utils.py
@@ -5,3 +5,16 @@ def q_search(query):
     #поиск по id
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+
+    #для полнотекстового поиска входные данные разбиваем на составляющие и формируем из них список из токенов запроса
+    keywords = [word for word in query.split() if len(word) > 2]
+    #создаем переменную как пустой Q объект для полнотекстового поиска
+    q_objects = Q()
+
+    for token in keywords:
+        #знак |= означает или равно и добавит Q объект в коллекцию в суммарное выражение Q объектов
+        q_objects |= Q(description__icontains=token)
+        #добавляем поиск по иммени
+        q_objects |= Q(name__icontains=token)
+        #применяем фильтр из Q объектов на методе object для формирования SQL запроса
+    return Products.objects.filter(q_objects)
