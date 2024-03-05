8840218f732869e8bf88776b2cb2df7faaa5db0a
skiner <fanomy90@gmail.com>
2024-03-05 23:17:06 +0300

    добавление корзины подключаемым шаблонном

--- store/users/views.py
+++ store/users/views.py
@@ -67,6 +67,9 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+def users_cart(request):
+    return render(request, "users/users_cart.html")
+
 #ограничение доступа для неавторизованных пользователей
 @login_required
 def logout(request):
