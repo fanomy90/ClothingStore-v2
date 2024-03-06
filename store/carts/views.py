5acdf4e65b0713e9ed030e57586bf76596f610ce
skiner <fanomy90@gmail.com>
2024-03-07 01:12:32 +0300

    добавление корзины в шаблоны сайта

--- store/carts/views.py
+++ store/carts/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from carts.models import Cart
+#from carts.utils import get_user_carts
 
-def cart_add(request, product_id):
+from goods.models import Products
+
+def cart_add(request, product_slug):
+    #получаем объект товара в корзину
+    product = Products.objects.get(slug=product_slug)
+    if request.user.is_authenticated:
+        #запрашиваем корзину пользователя по определенному товару
+        carts = Cart.objects.filter(user=request.user, product=product)
+        #если товар уже есть в корзине то увеличиваем количество товара на 1 и сохраняем корзину
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+            #если корзина по доавбляемому товару не найдена то создаем ее
+        else:
+            Cart.objects.create(user=request.user, product=product, quantity=1)
+    #редирект на страницу на которой был пользователь по ключу HTTP_REFERER - с какой страницы пришли
+    return redirect(request.META['HTTP_REFERER'])
+
+def cart_change(request, product_slug):
     ...
-def cart_change(request, product_id):
-    ...
-def cart_remove(request, product_id):
+def cart_remove(request, product_slug):
     ...
--- store/users/views.py
+++ store/users/views.py
@@ -17,7 +17,8 @@ def login(request):
                 #уведомление пользователя о входе
                 messages.success(request, f"{username}, успешно вошел на сайт")
                 #редирект для неавторизованного пользователя
-                if request.POST.get('next', None):
+                redirect_page = request.POST.get('netxt', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('main:index'))
     else:
